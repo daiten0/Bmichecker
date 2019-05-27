b4346bea8f035c5b0ab0f9efc0c690c50bee5882
Daisuke00 <dk424ksi17@gmail.com>
2019-05-27 18:05:20 +0900

    Initial Commit

---
+++ BmiChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BmiChecker
+//
+//  Created by daisuke on 2019/05/27.
+//  Copyright © 2019 Daisuke Kojyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
